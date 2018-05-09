e3573d273100a02e9d8e7923d408d803d17b3369
Lambd0x <lambdox@gmail.com>
2018-05-09 16:24:47 -0300

    Adicionadas funções para os problemas 11 e 27| ponto fixo ainda nao funciona problema 11 deve ser 0 e problema 27 deve 5.1655 aprox

--- trabalho-1-gustavo-nicolas.py
+++ trabalho-1-gustavo-nicolas.py
@@ -1,27 +1,38 @@
+# coding=utf8
 import math
 from decimal import *
-
+# Para que serve isso?
 getcontext().prec = 28
 
-#phi(x) = x + A(X) * f(x)
+# Funcao 11 --> Gustavo:
+# f0(x)    = 4x - sqrt(e^x) tem raíz entre (-0.0001, 0.0001) aparenta ser o 0
+# x        = sqrt(e^x)/4
+# phi0(x)  = sqrt(e^x)/4
+# phi0(x)' = sqrt(e^x) / 8
+# ---- Considerações no algoritmo
+# phi_gustavo  = phi0(x)
+# phiD_gustavo = phi0(x)'
 
-#Funcao Gustavo: 4x - sqrt(e^x) = 0
-#4x - sqrt(e^x) + x = x
-#5x - sqrt(e^x) = x
-#derivada: 5 - sqrt(e^x) / 2
+def phi_gustavo(x):
+    return Decimal.exp(x).sqrt() / 4
 
-#Funcao Nicolas: 1 - cost(x) * sqrt(x)
+def phiD_gustavo(x):
+    return Decimal.exp(x).sqrt() / 8
 
-def g_gustavo(x):
-    return x - f_gustavo(x) / fl_gustavo(x)
+# Funcao 27 --> Nicolas:
+# f1(x)     = 1 - cos(x)*sqrt(x) tem raíz entre (5.165, 5.166) aparenta ser o 5.1655
+# phi1(x)   = x + (1 - cos(x)sqrt(x))/sqrt(x)
+# phi1(x)   = x + 1/sqrt(x) - cos(x)
+# phi1(x)'  = sin(x) -1/(2*x^(2/3)) +1
+# ---- Considerações no algoritmo
+# phi_nicolas   = phi1(x)
+# phiD_nicolas  = phi1(x)'
 
-def fl_gustavo(x):
-    return (Decimal(5) - (Decimal.exp(x)).sqrt() / Decimal(2))
+def phi_nicolas(x):
+    return x + 1 / x.sqrt() - Decimal(math.cos(x))
 
-
-def f_gustavo(x):
-    return Decimal(4) * x - Decimal.exp(x).sqrt()
-
+def phiD_nicolas(x):
+    return Decimal(math.sin(x)) - 1 / ( 2 * x ** (2/3) ) + 1
 
 def ponto_fixo(pA, f, g, erro=Decimal("0.001"), max_iteracoes=100):
     k = 0
@@ -43,8 +54,6 @@ def ponto_fixo(pA, f, g, erro=Decimal("0.001"), max_iteracoes=100):
 
     return k, -1
 
-
 if __name__ == "__main__":
-
-    (n, xn) = ponto_fixo(2, f_gustavo, g_gustavo, Decimal("0.0000001"), max_iteracoes=5000)
+    (n, xn) = ponto_fixo(0.001, phi_nicolas, phiD_nicolas)
     print(n, xn)
