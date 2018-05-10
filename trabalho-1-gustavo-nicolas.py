0667695323401a082d4f05cf0dc32bb468a29116
Lambd0x <lambdox@gmail.com>
2018-05-09 23:39:10 -0300

    teste novo

--- trabalho-1-gustavo-nicolas.py
+++ trabalho-1-gustavo-nicolas.py
@@ -9,30 +9,32 @@ getcontext().prec = 28
 # x        = sqrt(e^x)/4
 # phi0(x)  = sqrt(e^x)/4
 # phi0(x)' = sqrt(e^x) / 8
-# ---- Considerações no algoritmo
-# phi_gustavo  = phi0(x)
-# phiD_gustavo = phi0(x)'
+
+def f_gustavo(x):
+    return 4*x - Decimal.exp(x).sqrt()
 
 def phi_gustavo(x):
     return Decimal.exp(x).sqrt() / 4
-
-def phiD_gustavo(x):
-    return Decimal.exp(x).sqrt() / 8
 
 # Funcao 27 --> Nicolas:
 # f1(x)     = 1 - cos(x)*sqrt(x) tem raíz entre (5.165, 5.166) aparenta ser o 5.1655
 # phi1(x)   = x + (1 - cos(x)sqrt(x))/sqrt(x)
 # phi1(x)   = x + 1/sqrt(x) - cos(x)
 # phi1(x)'  = sin(x) -1/(2*x^(2/3)) +1
-# ---- Considerações no algoritmo
-# phi_nicolas   = phi1(x)
-# phiD_nicolas  = phi1(x)'
 
-def phi_nicolas(x):
+def f_nicolas(x):
     return x + 1 / x.sqrt() - Decimal(math.cos(x))
 
-def phiD_nicolas(x):
-    return Decimal(math.sin(x)) - 1 / ( 2 * x ** (2/3) ) + 1
+def phi_nicolas(x):
+    return Decimal(math.sin(x)) - 1 / ( 2 * x ** Decimal(1.5) ) + 1
+
+
+def f_teste(x):
+    return 2 * x ** 3 - Decimal(11.7) * x ** 2 + Decimal(17.7) * x - 5
+
+def phi_teste(x):
+    return - Decimal(0.112) * x ** 3 + Decimal(0.66) * x ** 2 + Decimal(0.28)
+
 
 def ponto_fixo(pA, f, g, erro=Decimal("0.001"), max_iteracoes=100):
     k = 0
@@ -55,5 +57,5 @@ def ponto_fixo(pA, f, g, erro=Decimal("0.001"), max_iteracoes=100):
     return k, -1
 
 if __name__ == "__main__":
-    (n, xn) = ponto_fixo(0.001, phi_nicolas, phiD_nicolas)
+    (n, xn) = ponto_fixo(5.1, f_teste, phi_teste)
     print(n, xn)
